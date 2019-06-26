4e796c6ce70cd968e4ab2d67cfdf88b6bc9a2703
x430n <x@x430n.com>
2019-06-26 16:31:26 +0200

    Initial commit. Added skeloton of app, web,api and test-api projects. Created models. Added docker run shell script for running docker locally and db create sql script to create test db and default user for db.

---
+++ api/Models/InstantCoach.cs
@@ -0,0 +1,40 @@
+using System;
+
+namespace Api.Models
+{
+    public enum InstantCoachStatus
+    {
+        New = 1,
+        Waiting,
+        Updated,
+        InProgress,
+        Completed
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+    }
+
+    public class Template
+    {
+        public int Id { get; set; }
+    }
+
+    public class InstantCoach
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }        // varchar 1000
+        public InstantCoachStatus Status { get; set; } // tinyint(3)
+        public string TicketId { get; set; }           // varchar 64
+        public string Reference { get; set; }          // varchar 16
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+        public int EvaluatorId { get; set; }
+        public int AgentId { get; set; }
+        public int TemplateId { get; set; }
+        public string QuestionComments { get; set; }   // json
+        public string QuestionCount { get; set; }
+        public string BookmarkPins { get; set; }       // json
+    }
+}
---
+++ docker-run.sh
@@ -0,0 +1,3 @@
+sudo docker run -e 'ACCEPT_EULA=Y' -e 'SA_PASSWORD=Abc$12345' \
+   -p 1433:1433 --name sql1 \
+   -d microsoft/mssql-server-linux
