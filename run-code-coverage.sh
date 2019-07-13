2c493c198aec22cd34d3ceb3a8d564eaacdd3b8b
x430n <x@x430n.com>
2019-07-13 20:42:30 +0200

    All tests projects having coverlet for code coverage, I was thinking that it'll not be needed for tests-client-integrations but they test much of Startup and Middleware. Since ReportGenerator for code coverage is changing rapidly changed from hardcoded version to 4.*

--- run-code-coverage.sh
+++ run-code-coverage.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-dotnet test tests/tests-unit /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
-dotnet test tests/test-integration /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
+dotnet test /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
+#dotnet test tests/tests-unit /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
+#dotnet test tests/test-integration /p:CollectCoverage=true /p:CoverletOutputFormat=cobertura /p:CoverletOutput=./coverage.xml
 
 cd tests/tests-integration
 dotnet reportgenerator "-reports:coverage.xml;../tests-unit/coverage.xml" "-targetdir:../../coveragereport" "-assemblyfilters:-Tests.*"
